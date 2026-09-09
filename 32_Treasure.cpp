d9c09ba1e353e5565d167831b0cba28752b5003e
U. Nithin Sathya <unithinsathya@gmail.com>
2026-09-09 09:11:24 +0530

    08-09-2026 8th September

---
+++ 32_Treasure.cpp
@@ -0,0 +1,57 @@
+//https://codeforces.com/contest/2259/problem/E
+//https://codeforces.com/contest/2259/submission/389988700
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <set>
+using ll = long long;
+using namespace std;
+
+void solve()
+{
+    int N; cin >> N;
+    vector<int> arr(N+1), psum(N+1, 0);
+    for(int i=1; i<=N; i++) 
+    {
+        cin >> arr[i];
+        if(arr[i]>0)
+        {
+            psum[max(1, i-arr[i]+1)]++;
+            if(i+arr[i]<=N)
+            {
+                psum[i+arr[i]]--;
+            }
+        }
+    }
+    for(int i=1; i<=N; i++) psum[i] += psum[i-1];
+    string ans = "";
+    //Check Validity
+    for(int i=1; i<=N; i++)
+    {
+        if(arr[i]!=-1)
+        {
+            bool valid = false;
+            if(i-arr[i]>=1 && psum[i-arr[i]]==0) valid = true;
+            if(i+arr[i]<=N && psum[i+arr[i]]==0) valid = true;
+            if(!valid)
+            {
+                cout << -1 << endl; return;
+            }
+        }
+        char ch = (psum[i]==0 ?'1' :'0');
+        ans.push_back(ch);
+    }
+
+    cout << ans << endl;
+
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc; cin >> tc;
+    while(tc--) solve();
+    return 0;
+}
